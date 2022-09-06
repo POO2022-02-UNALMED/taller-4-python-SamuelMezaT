dbbbd50a35c2c723e98162da7208917c1a9a654e
SamuelMezaT <smezat@unal.edu.co>
2022-09-05 20:26:45 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,41 +1,29 @@
-
-
 from classroom.asignatura import Asignatura
-from classroom.asignatura import Asignatura
-
-
 class Grupo:
-
-    grado = None
-
-    def __init__(self, grupo="grupo predeterminado", asignaturas=[] grado=None, estudiantes=None):
+    grado = "Grado 12"
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
-        self.grado = "Grado "+str(grado)
-
     def listadoAsignaturas(self, **kwargs):
+        a=[]
         for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
-
+            a.append(Asignatura(x))
+        self._asignaturas=a
     def agregarAlumno(self, alumno, lista=[]):
         if(lista is None):
             lista.append(alumno)
             self.listadoAlumnos = self.listadoAlumnos + lista
         else:
-            self.listadoAlumnos = lista + [alumno]
-
-    def __str__(self):
-        return "Grupo de estudiantes: "+ self._grupo
-
+            self.listadoAlumnos =  lista + [alumno]
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
         cls.grado = nombre
-
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
-
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
+    def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
+    def __str__(self):
+        return "Grupo de estudiantes: "+self._grupo
